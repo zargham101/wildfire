307435c7cc011a0a38e64f7557d156a07c2aed00
unknown <khanzargham434@gmail.com>
2025-01-19 13:38:28 -0800

    added landingPage footer and navbar

--- frontend/wildfire/src/App.js
+++ frontend/wildfire/src/App.js
@@ -1,8 +1,13 @@
-
+import React from "react";
+import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
+import LandingPage from "./components/LandingPage";
 function App() {
   return (
     <div className="App">
-      
+      <Navbar/>
+      <LandingPage/>
+      <Footer/>
     </div>
   );
 }

e878e1731b89fd235130f884797ffe47635eeddb
unknown <khanzargham434@gmail.com>
2025-04-23 02:21:04 +0500

    Update: Navbar and footer font updated

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -8,11 +8,11 @@ const Footer = () => {
       <div className="max-w-screen-xl mx-auto flex flex-col items-center text-center">
         <div className="flex items-center space-x-2 mb-4">
           <img src="/images/logo.png" alt="FireZon Logo" className="h-10" />
-          <span className="text-orange-500 text-2xl font-bold">
+          <span className="text-orange-500 font-serif text-2xl font-bold">
             WildFire Watch
           </span>
         </div>
-        <p className="text-sm mb-4">
+        <p className="text-sm mb-4 font-serif">
           Fire departments are organization within municipality county.
         </p>
         <div className="flex space-x-6 mb-4">
@@ -30,7 +30,7 @@ const Footer = () => {
           </Link>
         </div>
         <div className=" w-[1345px] border-t-2 border-orange-500 mt-8">
-          <div className="text-sm text-gray-400 pt-4">
+          <div className="text-sm text-gray-400 pt-4 font-serif">
             <p>&copy; 2025 - All Rights Reserved | Created By Hassnain & Kainat</p>
           </div>
         </div>

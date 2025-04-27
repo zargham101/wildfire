8660ffb5f2954caa041d9f7b61cf98554cb66f79
unknown <khanzargham434@gmail.com>
2025-04-27 23:11:05 +0500

    Merge changes from stash: updated App.js, Navbar, Footer, and index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,15 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import { GoogleOAuthProvider } from "@react-oauth/google";
+import { AuthProvider } from "./routes/AuthContext";
 import "./styles/index.css";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <GoogleOAuthProvider clientId="805440075167-1uikcp7qrpdsoj8a9ldpf1arhhto02el.apps.googleusercontent.com">
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </AuthProvider>
   </GoogleOAuthProvider>,
   document.getElementById("root")
 );
